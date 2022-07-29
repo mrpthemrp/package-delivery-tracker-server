e036ba43f482362ab25d3da406d1022b7681b1f4
Deborah Wang <deborah.0214@hotmail.com>
2022-07-29 14:34:31 -0700

    Finish push spring boot

---
+++ src/main/java/cmpt213/assignment4/packagedeliveries/webappserver/WebAppServerApplication.java
@@ -0,0 +1,13 @@
+package cmpt213.assignment4.packagedeliveries.webappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppServerApplication.class, args);
+	}
+
+}
