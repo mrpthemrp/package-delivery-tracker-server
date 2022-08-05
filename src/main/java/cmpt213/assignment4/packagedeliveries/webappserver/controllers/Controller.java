086b9ad3e6c125b7accc5712d487ba118c8091a8
Deborah Wang <deborah.0214@hotmail.com>
2022-08-04 21:18:30 -0700

    Add controller class

---
+++ src/main/java/cmpt213/assignment4/packagedeliveries/webappserver/controllers/Controller.java
@@ -0,0 +1,8 @@
+package cmpt213.assignment4.packagedeliveries.webappserver.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+}
